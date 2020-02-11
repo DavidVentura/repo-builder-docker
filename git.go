d2c52768c1842d3099e5691bed087c0baf6c34f8
david <davidventura27@gmail.com>
2020-02-11 15:13:35 +0100

    cleanup repo when operating on it

--- git.go
+++ git.go
@@ -7,19 +7,34 @@ import (
 	"os/exec"
 )
 
-func cloneRepo(repo Repo, output io.Writer) error {
+func cloneRepo(repo Repo, hookData HookData, output io.Writer) error {
 	output.Write([]byte(fmt.Sprintf("Requested to clone %s\n", repo)))
+	commands := [][]string{
+		{"clean", "-fd"},
+		{"reset", "--hard"},
+		{"checkout", "master"},
+		{"pull"},
+		{"checkout", hookData.Tag},
+	}
 	if _, err := os.Stat(repo.dstPath); os.IsNotExist(err) {
-		cloneCmd := exec.Command("git", "clone", repo.GitUrl, repo.dstPath)
-		out, err := cloneCmd.CombinedOutput()
-		output.Write([]byte(fmt.Sprintf("Output of git clone of %s", repo)))
-		output.Write([]byte(out))
+		commands = append([][]string{{"clone", repo.GitUrl, repo.dstPath}}, commands...)
+		err = os.Mkdir(repo.dstPath, 0700)
 		if err != nil {
-			output.Write([]byte(fmt.Sprintf("Git clone of %s failed: %s\n", repo, err)))
 			return err
 		}
 	} else {
 		output.Write([]byte("No need to clone the repository as it exists already\n"))
+	}
+
+	for _, command := range commands {
+		cmd := exec.Command("git", command...)
+		cmd.Dir = repo.dstPath
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return err
+		}
+		output.Write([]byte(fmt.Sprintf("Output of %s of %s\n", cmd.String(), repo)))
+		output.Write([]byte(out))
 	}
 	return nil
 }
